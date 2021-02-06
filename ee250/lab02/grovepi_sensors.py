52c079f59fa5482149e719701c4272695aa2a470
Jeffrey Liu <jliu8288@usc.edu>
2021-02-06 05:47:48 +0000

    demo

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -43,11 +43,12 @@ if __name__ == '__main__':
         #print(grovepi.ultrasonicRead(PORT))
 	#print("sensor_value =%d" %(sensor_value))
         print(sensor_value)
-        setRGB(0,255,0)
-        setText_norefresh(str(sensor_value) + "cm " + "\n"  + str(ultra_value) + "cm " )
-        if(sensor_value >  ultra_value):
-           setText_norefresh(str(sensor_value) + "cm OBJ PRES " + "\n"+ str(ultra_value) + "cm ")
-           setRGB(255,0,0)
+        while(sensor_value < ultra_value):
+            setRGB(0,255,0)
+            setText_norefresh(str(sensor_value) + "cm " + "\n"  + str(ultra_value) + "cm " )
+        while(sensor_value >  ultra_value):
+            setText_norefresh(str(sensor_value) + "cm OBJ PRES " + "\n"+ str(ultra_value) + "cm ")
+            setRGB(255,0,0)
         #setText_norefresh(str(grovepi.ultrasonicRead(PORT)) + "cm ")
 
 
