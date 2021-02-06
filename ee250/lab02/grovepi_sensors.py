a12f666d940e226f95a2a0607a8b62dc1c732698
Jeffrey Liu <jliu8288@usc.edu>
2021-02-05 19:11:55 -0800

    lab02

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -41,6 +41,6 @@ if __name__ == '__main__':
         #print(grovepi.ultrasonicRead(PORT))
 	#print("sensor_value =%d" %(sensor_value))
         print("sensor_value")
-        setText("Hello World")
+        setText("sensor_value")
 	
 
